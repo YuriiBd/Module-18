0cb5fee0d73b23d3c20c7152c8f22f5e0a06d8e7
iurii11 <iu11@yandex.ru>
2021-06-16 23:53:49 +0300

    added final project CurrencyBot

---
+++ app.py
@@ -0,0 +1,54 @@
+import telebot
+from config import keys, TOKEN
+from utils import ConvertionException, CurrencyConverter
+
+# @Yuri_ChangeCurrencyBot
+
+bot = telebot.TeleBot(TOKEN)
+
+
+# handler start help
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате: \n \
+<имя валюты> \n \
+<в какую валюту перевести> \n \
+<количество переводимой валюты> \n увидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+# handler values to take currency
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+# conversion currency
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров')
+        quote, base, amount = values
+        quote = quote.lower()
+        base = base.lower()
+        total_base = CurrencyConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+
+    else:
+        def toFixed(numObj, digits=0):
+            return f"{numObj:.{digits}f}"
+
+        text = f'Цена {int(amount)} {quote} в {base} - {toFixed(total_base, 2)} {base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '1852282410:AAGgweL8FQoTzYk8DBQ97WAJwlSiHxaWf5g'
+
+keys = {
+    'рубль': 'RUR',
+    'доллар': 'USD',
+    'евро': 'EUR',
+}
